9dbffb11f65423a3be0ac0523e7fe3233decbbb6
Maulen <azikulov@proton.me>
2026-09-21 00:19:02 +0500

    chore: добавлена конфигурация для безопасного получения переменных из файла .env

---
+++ app/app.ts
@@ -0,0 +1,3 @@
+import { env } from "./common/config";
+
+console.log(env.BOT_TOKEN);
---
+++ app/common/config/env.ts
@@ -0,0 +1,24 @@
+import "dotenv/config";
+import process from "node:process";
+import * as v from "valibot";
+
+type EnvData = v.InferOutput<typeof envSchema>;
+
+const envSchema = v.object({
+  BOT_TOKEN: v.pipe(
+    v.string("Переменная BOT_TOKEN должна быть строкой!"),
+    v.minLength(1, "Переменная BOT_TOKEN не найдена!"),
+  ),
+  ADMIN_IDS: v.pipe(
+    v.string("Переменная ADMIN_IDS должна быть строкой!"),
+    v.minLength(1, "Переменная ADMIN_IDS не найдена!"),
+  ),
+});
+
+const safeEnv = v.safeParse(envSchema, process.env);
+
+safeEnv.issues?.forEach((issue) => {
+  throw Error(issue.message);
+});
+
+export const env = safeEnv.output as EnvData;
